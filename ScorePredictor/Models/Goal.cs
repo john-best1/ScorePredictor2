22b993cd0996ef6450b3eebe1a49424b8583f19e
john-best1 <besty_2006@hotmail.co.uk>
2020-01-06 16:03:18 +0000

    Goal Model

---
+++ ScorePredictor/Models/Goal.cs
@@ -0,0 +1,11 @@
+﻿namespace ScorePredictor.Models
+{
+    public class Goal
+    {
+        public int minute { get; set; }
+
+        public string scorer { get; set; }
+
+        public string assist { get; set; } = "";
+    }
+}
